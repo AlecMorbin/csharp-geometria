c5b216d22fdabee3bf9ccda6ce0565d346c287a6
Alec <alemor98@hotmail.it>
2022-04-07 14:55:55 +0200

    Completato disegno e testato nel main

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -8,5 +8,8 @@ Console.WriteLine("Inserisci la base e successivamente l'altezza: ");
 int baseR = int.Parse( Console.ReadLine() );
 int altezzaR = int.Parse( Console.ReadLine() );
 rettangolo = new Rettangolo(baseR, altezzaR);
+//testing tostring
 Console.WriteLine(rettangolo.ToString());
 
+//testing Disegno
+Console.WriteLine(rettangolo.Disegna());
--- Geometria/Rettangolo.cs
+++ Geometria/Rettangolo.cs
@@ -73,5 +73,41 @@ namespace Geometria
                     "\nPerimetro: " + calcolaPerimetro() + " cm"+
                     "\nArea: " + calcolaArea() + " cm^2";
         }
+
+        public string Disegna()
+        {
+            string disegno = "";
+            //separata la costruzione del disegno in componennti per renderlo più chiaro e modulare
+            disegno += disengaBordo(getBase());
+            //richiamo i metodi per costruire il disegno e per il centro ciclo tante volte quante è alto il disegno
+            for (int i = 0; i < getAltezza(); i++) 
+                disegno += disengaCentrale(getBase()); 
+            disegno += disengaBordo(getBase());
+
+            return disegno;
+        }
+
+        //l'esterno non ha bisogno di vedere questi metodi perciò saranno privati
+        private string disengaCentrale(int lunghezza)
+        {
+            string result = "|";
+            //le barrela laterali rimangono statiche e per lunghezza orizzontale aggiungo spazi o -- 
+            for (int i = 0; i < lunghezza; i++)
+                result += "  ";
+            result += "|\n";
+
+            return result;
+        }
+
+        private string disengaBordo(int lunghezza)
+        {
+            string result = "|";
+
+            for (int i = 0; i < lunghezza; i++)
+                result += "--";
+            result += "|\n";
+
+            return result;
+        }
     }
 }
