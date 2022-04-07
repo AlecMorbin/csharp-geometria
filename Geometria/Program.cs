1e10e50f2978006a66b8313d3266acdb24a422e6
Alec <alemor98@hotmail.it>
2022-04-07 14:33:39 +0200

    Creazione Classe e prima prove nel Main

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Geometria
+
+using Geometria;
+
+Rettangolo rettangolo;
+
+Console.WriteLine("Inserisci la base e successivamente l'altezza: ");
+int baseR = int.Parse( Console.ReadLine() );
+int altezzaR = int.Parse( Console.ReadLine() );
+rettangolo = new Rettangolo(baseR, altezzaR);
+Console.WriteLine(rettangolo.ToString());
---
+++ Geometria/Rettangolo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    internal class Rettangolo
+    {
+        //attributi
+        private int baseRettangolo;
+        private int altezzaRettangolo;
+
+        //costruttore 
+
+        public Rettangolo(int baseR, int altezzaR)
+        {
+            setBase(baseR);
+            setAltezza(altezzaR);
+        }
+
+        //metodi
+
+        public int getBase()
+        {
+            return baseRettangolo;
+        }
+
+        public void setBase(int baseR)
+        {
+            if (baseR<0)
+            {
+                this.baseRettangolo = 0;
+            }else
+            {
+                this.baseRettangolo = baseR;
+            }
+        }
+
+        public int getAltezza()
+        {
+            return altezzaRettangolo;
+        }
+
+        public void setAltezza(int altezzaR)
+        {
+            if (altezzaR < 0)
+            {
+                this.altezzaRettangolo=0;
+            }
+            else
+            {
+                this.altezzaRettangolo = altezzaR;
+            }
+        }
+
+        public float calcolaArea()
+        {
+            return getBase()*getAltezza();
+        }
+
+        public int calcolaPerimetro()
+        {
+            return (getBase()+getAltezza())*2;
+        }
+
+        public string ToString()
+        {
+            return "Base: " + getBase() +
+                    "Altezza: " + getAltezza() +
+                    "Perimetro: " + calcolaPerimetro() +
+                    "Area: " + calcolaArea();
+        }
+    }
+}
